edcefcab8c0a9f0631404e90800e88d7f7a7ead3
ethanium <ethanium@live.com>
2020-10-15 17:16:49 +0800

    20201015

---
+++ githubapi/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using githubapi.Models; 
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace githubapi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        [HttpGet, HttpPost]
+        public async Task<IActionResult> Index()
+        {
+            if (Request.Query.ContainsKey("q"))
+            {
+                string q = Request.Query["q"].ToString().ToLower();
+                List<string> userNames = q.Split(',').Select(x => x.Trim()).ToList();
+
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response =
+                        await client.PostAsync(
+                            "http://localhost:2685/v1/users/retrieveUsers",
+                            new StringContent(JsonConvert.SerializeObject(userNames),
+                            Encoding.UTF8,
+                            "application/json"));
+
+                    byte[] buffer = await response.Content.ReadAsByteArrayAsync();
+                    byte[] byteArray = buffer.ToArray();
+                    string content = Encoding.UTF8.GetString(byteArray, 0, byteArray.Length);
+                    List<GitHubUser> users = JsonConvert.DeserializeObject<List<GitHubUser>>(content);
+                    ViewBag.Result = users;
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ githubapi/Controllers/v1/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using githubapi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace githubapi.Controllers.v1
+{
+    [Route("v1/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet, HttpPost]
+        [Route("retrieveUsers")]
+        public async Task<ActionResult<List<GitHubUser>>> RetrieveUsers(List<string> userNames)
+        {
+            List<GitHubUser> users = new List<GitHubUser>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("User-Agent", "GitHubUsers");
+
+                HttpResponseMessage response = await client.GetAsync($"https://api.github.com/users/{userNames.FirstOrDefault()}");
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    byte[] buffer = await response.Content.ReadAsByteArrayAsync();
+                    byte[] byteArray = buffer.ToArray();
+                    string content = Encoding.UTF8.GetString(byteArray, 0, byteArray.Length);
+                    GitHubUser user = JsonConvert.DeserializeObject<GitHubUser>(content);
+                    users.Add(user);
+                }
+            }
+
+            return users;
+        }
+    }
+}
